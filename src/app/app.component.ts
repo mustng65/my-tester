937d8e2261e0e81c8fa33e26f5dd69ab027ed389
David <mustng65@hotmail.com>
2023-11-15 22:01:30 -0800

    add stuff

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { MyLibService } from 'my-lib';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'my-tester';
+  constructor(private test: MyLibService) {
+    test.getSomethingCool().subscribe((res) => console.log(res));
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MyLibModule } from "my-lib";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MyLibModule.forRoot({url:'https://splunk',token:'splunk-token-to-use'})
   ],
   providers: [],
   bootstrap: [AppComponent]
